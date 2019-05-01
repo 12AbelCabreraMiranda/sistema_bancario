27200fb62bcf738590fd5d5fd5f162cb50b19499
12AbelCabreraMiranda <leg.abelr@gmail.com>
2019-04-30 19:05:54 -0600

    disenio desarrollado del modulo RECEPTOR PAGADOR, Primer commit

--- receptorPagador/receptorPagador.php
+++ receptorPagador/receptorPagador.php
@@ -1,24 +1,49 @@
-<?php 
+
+<?php
+    require_once ("../crear_cuenta/conexion.php");
     session_start();
-    if(!isset($_SESSION["user"])){
-    header("location:../login/index.php");
+
+    $usuarioLogeado = $_SESSION['user'];
+    
+    $id_empleadoBancoLogeado;
+    $consulta = ("SELECT banco_id_empleado FROM empleado where nombre='$usuarioLogeado'");
+    $resultado = $con->query($consulta);
+    if($row = $resultado->fetch_assoc()){          
+        $id_empleadoBancoLogeado=$row['banco_id_empleado'];
     }
-
-  ?>
-
+    $nombre_empleadoBancoLogeado;
+    $consulta2 = ("SELECT nombre_banco FROM banco where id_banco='$id_empleadoBancoLogeado'");
+    $resultado2 = $con->query($consulta2);
+    if($row = $resultado2->fetch_assoc()){          
+        $nombre_empleadoBancoLogeado=$row['nombre_banco'];
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mi Banco</title>
+    <title>Receptor Pagador</title>
     <link rel="icon" href="../img/icono.png">
     <link rel="stylesheet" href="../login/Resources/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
 
 </head>
  
+ 
+    
 <body>
+
+    <?php       
+        if(isset($_SESSION['user'])){
+        }else{
+            header("location:../login/index.php");
+        }
+    ?>
     <!--Barra de Navegacion-->
     <nav class="navbar navbar-default">
         <div class="navbar-header">
@@ -28,7 +53,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="#" class="navbar-brand">Sistema Bancario</a>
+            <a href="receptorPagador.php" class="navbar-brand">Sistema Bancario: <?php  echo $row['nombre_banco']; ?></a>
         </div>
 
         <div class="collapse navbar-collapse navbar-ex1-collapse">
@@ -43,21 +68,107 @@
             </ul>
         </div>
     </nav>
+    <center>
+        <div style="margin-left:20px"><h4>Módulo RECEPTOR PAGADOR </h4></div>
+    </center>
+
+    <!--FORMULARIOS -->
     <div class="container">
         <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Bienvenido</div>
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div class="panel panel-info">
+                    <div class="panel-heading"><h4>Depósitos</h4></div>
                     <div class="panel-body">                     
-                      <h1>Módulo ADMINISTRADOR DEL SISTEMA</h1>
+                        
+                        <form action="misAlumnos.php" method="post" id="formDatosEstudiantes" enctype="multipart/form-data">                                                   
+                            <div class='col-sm-5'>
+                                <label for="nombre">Número de cuenta</label> 
+                                
+                                <div class="input-group col-md-12">
+                                    <input REQUIRED name="numeroCuenta" type="text" class="form-control" placeholder="Ej. 32892928"/>
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-info" type="button">
+                                            <span class="glyphicon glyphicon-search"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <p></p>
+                                    <label for="nombre">¿Cuánto va a depositar?</label>                        
+                                    <input REQUIRED name="cantidad" class="form-control" id="cantidad" type="text" placeholder="Q. 00.00">
+                                </div>
+                                <!-- Respuesta de la base de datos-->
+                                <div class="alert alert-success alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <strong>Éxito!  </strong>  Se ha registrado el depósito 
+                                </div>
+                                   
+                                
+                            </div>
+
+                            <div class='col-sm-7' id="datoCuenta">
+                                <h4 class="text-center">Datos de la cuenta</h4>
+                                <p>No. de cuenta: <b> 123456789</b></p>
+                                <p>Cliente: <b> Tomy Vargas</b></p>
+                                <p>Banco: <b> Banrural</b></p>
+                                
+                            </div>
+                            
+                            <div class="form-group pull-left">
+                                <center> <br>
+                                    <input  onclick="registrarAlumno()" id="boton" type="submit" class="btn btn-info" value="Realizar depósito">                       
+                                </center> 
+                            </div>
+                        </form>                                                    
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Menú lateral isquierdo -->
+    <div id="wrapper">
+        <div class="overlay"></div>    
+        <!-- Menú lateral isquierdo -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                       Receptor Pagador
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Realizar Depósito</a>
+                </li>
+                <li>
+                    <a href="#">Realizar Retiros</a>
+                </li>
+                <li>
+                    <a href="#">Realizar Transacciones</a>
+                </li>
+                <li>
+                    <a href="#">Cambio de dólares</a>
+                </li>                                                              
+            </ul>
+        </nav>        
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper" >
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas" style="margin-top:40px">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>            
+        </div>        
+    </div>
+
     <script src="../login/Resources/js/jquery-1.11.2.js"></script>
     <script src="../login/Resources/js/bootstrap.min.js"></script>
+    <script src="js/evento.js"></script>
+
     <script>
         function cerrar()
         {
