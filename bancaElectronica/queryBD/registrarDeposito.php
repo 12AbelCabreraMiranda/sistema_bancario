6cc901734d8f47ff1ac7a5822d64d951e6ea3a16
12AbelCabreraMiranda <leg.abelr@gmail.com>
2019-05-05 04:16:05 -0600

    programando transferencia de saldo - la logica de transferencia de salto completado

--- bancaElectronica/queryBD/registrarDeposito.php
+++ bancaElectronica/queryBD/registrarDeposito.php
@@ -15,10 +15,12 @@
      }
     
     $cuentaCLienteLogeado;
-    $consulta = ("SELECT numero_de_cuenta FROM cuenta_cliente_logueado where usuario_cliente='$usuarioLogeado'");
+    $mi_saldoLogueado;
+    $consulta = ("SELECT numero_de_cuenta, saldo_actual FROM cuenta_cliente_logueado where usuario_cliente='$usuarioLogeado'");
     $resultado = $con->query($consulta);
     if($row = $resultado->fetch_assoc()){          
-        $cuentaCLienteLogeado=$row['numero_de_cuenta'];
+        $cuentaCLienteLogeado=$row['numero_de_cuenta'];//cuenta logueada
+        $mi_saldoLogueado=$row['saldo_actual'];//saldo logueado
 
         if($cuentaCLienteLogeado==$cuenta_a_transferir){        
         
@@ -30,18 +32,53 @@
                     No se puede realizar la transferencia en tu misma cuenta... MUY LOCO ..  XD
                 </div>';
         }else{   
-            //actualizar ya funciona pero falta validar mi saldo para poder transferir
-            //$query2 = "UPDATE chequeras SET saldo_actual=saldo_actual+'$cantidadTransferido' where cuenta_id_chequera='$idCuentaTransferir' ";
-            //$resultad2= $con->query($query2);                  
-            echo '<div class="alert alert-success alert-dismissible" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <strong>EXCELENTE..!  </strong> <br> 
-                    La transferencia de saldo fue un éxito.
-                </div>';
+            
+            if($cantidadTransferido==0){                
+                echo '<div class="alert alert-warning alert-dismissible" role="alert" style="background-color: #f9e572">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <strong>Advertencia!  </strong>      Transfiera por lo menos Q.1.00  jajajajaja XD                        
+                    </div>';
+            }else if($cantidadTransferido>$mi_saldoLogueado){
+                if($mi_saldoLogueado==0){
+                   
+                    echo '<div class="alert alert-warning alert-dismissible" role="alert" style="background-color: #f9e572">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <strong>Advertencia! Su saldo está en cero  </strong>                              
+                        </div>';
+                }else if($mi_saldoLogueado>0){
+                    
+                    echo '<div class="alert alert-warning alert-dismissible" role="alert" style="background-color: #f9e572">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <strong>Advertencia!   </strong>   No tiene el dinero suficiente para transferir   :(                           
+                        </div>';
+                }
+            }else if($cantidadTransferido<=$mi_saldoLogueado){
+                    
+                    //actualizar querys
+                    //actualizar ya funciona pero falta validar mi saldo para poder transferir
+                    $query2 = "UPDATE chequeras SET saldo_actual=saldo_actual+'$cantidadTransferido' where cuenta_id_chequera='$idCuentaTransferir' ";
+                    $resultad2= $con->query($query2);   
+                    
+                    //restar de mi saldo
+                    $query5 = "UPDATE chequeras SET saldo_actual=saldo_actual-'$cantidadTransferido' where numero_de_cuenta='$cuentaCLienteLogeado' ";
+                    $resultad5= $con->query($query5);
+                    echo '<div class="alert alert-success alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                            <strong>EXCELENTE..!  </strong> <br> 
+                            La transferencia de saldo fue un éxito.
+                        </div>';
+    
+                        
+                }/*
 
-                /*
             //HORA SISTEMA AL REGISTRARSE
             ini_set('date.timezone', 'America/Guatemala');
             $hora_sistema = date ('H:i:s', time());
