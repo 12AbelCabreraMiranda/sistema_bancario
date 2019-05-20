a421a13f16cd890b69aa3232c35b62e5b4d393fc
12AbelCabreraMiranda <leg.abelr@gmail.com>
2019-05-20 16:18:48 -0600

    trabajando con QR para contrasenia

--- crear_cuenta/codigoBancaVirtual/pdf-template/welcome.php
+++ crear_cuenta/codigoBancaVirtual/pdf-template/welcome.php
@@ -45,6 +45,45 @@
 					Usuario <?php  echo $row['usuario_cliente']; ?> 
 					<br/>
 					Contraseña : <?php  echo $row['contrasenia_cliente']; ?>
+					<div>mostrar codigo QR here
+						<?php
+							$conexion = new mysqli("localhost","root","","sistema_bancario");
+
+							$usu;
+							$query = "SELECT  nombre from clientes where id_clientes=1";
+							$resultado = $conexion->query($query);
+							if($row = $resultado->fetch_assoc()){
+								$usu= $row['nombre'];
+							} 
+							//Agregamos la libreria para genera códigos QR
+							require "phpqrcode/qrlib.php";    
+
+							//Declaramos una carpeta temporal para guardar la imagenes generadas
+							$dir = 'temp/';
+
+							//Si no existe la carpeta la creamos
+							if (!file_exists($dir))
+								mkdir($dir);
+
+								//Declaramos la ruta y nombre del archivo a generar
+							$filename = $dir.'test.png';
+
+								//Parametros de Condiguración
+
+							$tamaño = 10; //Tamaño de Pixel
+							$level = 'L'; //Precisión Baja
+							$framSize = 3; //Tamaño en blanco
+							//$Contraseña = "Cabrera"; //Texto
+
+								//Enviamos los parametros a la Función para generar código QR 
+							QRcode::png($usu, $filename, $level, $tamaño, $framSize); 
+
+								//Mostramos la imagen generada
+							echo '<img src="'.$dir.basename($filename).'" /><hr/>';  
+						?>
+
+
+					</div>
 				</div>
 				<div class="panel-body">Con estos datos Usted podrá acceder al uso de la Banca Virtual desde su dispositivo en cualquier momento.
 					<p>- Usted podrá consultar saldo</p>
