d02290b61c9d9ed789d5533b192eece34dcf12d2
12AbelCabreraMiranda <leg.abelr@gmail.com>
2019-05-20 15:22:27 -0600

    mejorando interface de pdf

--- crear_cuenta/codigoBancaVirtual/pdf-template/welcome.php
+++ crear_cuenta/codigoBancaVirtual/pdf-template/welcome.php
@@ -7,7 +7,7 @@
 	
     
 		//TRAER DATOS		
-		$querySelect = ("SELECT usuario_cliente, contrasenia_cliente, nombre,apellido,numero_de_cuenta,hora_solicitado,fecha_solicitado  from vista_usuarios_banca_virtual where numero_de_cuenta ='$cuentaClienteLog' ");
+		$querySelect = ("SELECT usuario_cliente, contrasenia_cliente, nombre,apellido,numero_de_cuenta,hora_solicitado,fecha_solicitado,nombre_tipoCuenta  from vista_usuarios_banca_virtual where numero_de_cuenta ='$cuentaClienteLog' ");
 		$resultSelect =$con->query($querySelect);
 		if($row = $resultSelect->fetch_assoc()){
 			$usu=$row['usuario_cliente'];
@@ -17,7 +17,7 @@
 			$numeroCuenta=$row['numero_de_cuenta'];
 			$hora=$row['hora_solicitado'];
 			$fecha=$row['fecha_solicitado'];
-
+			$tipoCuenta=$row['nombre_tipoCuenta'];
 		  
 		}
 
@@ -28,22 +28,29 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
 
 	<div class="container-fluid">
-		<div class="panel panel-success">
-		
-			<div class="panel-heading">
-			<h3>Nombre del cliente: <?php  echo $row['nombre']; ?> &nbsp; <?php echo $row['apellido']; ?> </h3>
-				NUMERO DE CUENTA: <?php  echo $row['numero_de_cuenta']; ?> 
-				<br/> <br>
-				Usuario <?php  echo $row['usuario_cliente']; ?> 
-				<br/>
-				Contraseña : <?php  echo $row['contrasenia_cliente']; ?>
-			</div>
-			<div class="panel-body">Con estos datos Usted podrá acceder al uso de la Banca Virtual.
-				<p>Fecha que fue solicitado estos datos: <?php  echo $row['hora_solicitado']; ?> del <?php  echo $row['fecha_solicitado']; ?></p>
+		<div class="col-md-8 col-md-offset-2">
+
+			<div class="panel panel-success">
+			<h2 class="text-center">BANCA ELECTRÓNICA</h2>
+				<div class="panel-heading">
+				<h3>Nombre del cliente: <?php  echo $row['nombre']; ?> &nbsp; <?php echo $row['apellido']; ?> </h3>
+					NUMERO DE CUENTA: <?php  echo $row['numero_de_cuenta']; ?> <br>
+					TIPO DE CUENTA: <?php  echo $row['nombre_tipoCuenta']; ?> <br>
+					<br/> <br>
+					Usuario <?php  echo $row['usuario_cliente']; ?> 
+					<br/>
+					Contraseña : <?php  echo $row['contrasenia_cliente']; ?>
+				</div>
+				<div class="panel-body">Con estos datos Usted podrá acceder al uso de la Banca Virtual desde su dispositivo en cualquier momento.
+					<p>- Usted podrá consultar saldo</p>
+					<p>- Realizar Transaferencia de saldo a cualquier cuenta de otro banco</p> <hr>
+					<p>Fecha que fue solicitado estos datos: <?php  echo $row['hora_solicitado']; ?> del <?php  echo $row['fecha_solicitado']; ?></p>
+				</div>
 			</div>
 		</div>
 	</div>
